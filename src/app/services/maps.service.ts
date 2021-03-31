7b1dc8f86936bdfe3b0dfcca752efd84655b5307
samantha202 <nsamanthaduclaire@gmail.com>
2021-03-31 12:11:11 +0200

    geocoding perfect

--- src/app/services/maps.service.ts
+++ src/app/services/maps.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
 
+//variable pour stocker les différentes valeurs du Geocoding
+ var geocoder: any;
+ var ville: any;
+ var code_postal: string;
+ var rue: string;
+ var numero: string;
+ var departement:string;
+ var latitude:string;
+ var longitude:string;
+ var adresse : any;
 @Injectable({
   providedIn: 'root'
 })
-export class MapsService {
-
+export class MapsService { 
   constructor() { }
+  //geocoding
+  geocoding(adr:string) {
+  adresse = adr;
+  geocoder = new google.maps.Geocoder();
+    geocoder.geocode({'address': adr}, function(results:any) {
+      ville = results[0].address_components[2].long_name;
+      code_postal = results[0].address_components[6].long_name;
+      rue = results[0].address_components[1].long_name;
+      numero = results[0].address_components[0].long_name;
+      departement = results[0].address_components[4].long_name;
+      latitude = results[0].geometry.location.lat();
+      longitude = results[0].geometry.location.lng();
+    })
+    return geocoder;
+  }
 }
--- src/app/views/searchr/searchr.component.ts
+++ src/app/views/searchr/searchr.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { GooglePlaceModule, GooglePlaceDirective } from "ngx-google-places-autocomplete";
+import { MapsService } from 'src/app/services/maps.service';
+
+//variable por stocker les différentes valeurs du Geocoding
+var geocoder: any;
+var marker : any;
+var infowindow:any;
+var infowindowContent: any;
+var map: any;
+var marker:any;
+var adresse : any;
 
 @Component({
   selector: 'app-searchr',
@@ -7,14 +17,15 @@ import { GooglePlaceModule, GooglePlaceDirective } from "ngx-google-places-autoc
   styleUrls: ['./searchr.component.css']
 })
 export class SearchrComponent implements OnInit {
-
+  adresse = '';
   google: any ;
-map: any;
-  constructor() { }
+  map: any;
+  constructor(private maps:MapsService) { }
 
   ngOnInit(): void {
-    this.initMap();
+    this.initMap(); //j'initialise ma carte au préalable sinon il ne sera pas visible
   }
+  //cette fonction permet d'initialiser ma carte 
   initMap(){
     var latlng = new google.maps.LatLng(46.52863469527167,2.43896484375);
     var mapOptions = {
@@ -25,6 +36,46 @@ map: any;
     const getElement = document.getElementById('mapp-canvas');
     if(getElement){
       this.map = new google.maps.Map(getElement, mapOptions);
+      map = this.map;
     } 
+  }
+  doGeocoding()
+  {
+    this.adresse = (<HTMLInputElement>document.getElementById('adresse')).value;
+    adresse = this.adresse;
+    geocoder = this.maps.geocoding(this.adresse);
+    this.SearchAdress();
+  }
+    SearchAdress() {
+      const self = this;
+      infowindow = new google.maps.InfoWindow();
+      infowindowContent = document.getElementById('infowindow-content');
+      infowindow.setContent(infowindowContent); 
+      geocoder.geocode({ 'address':this.adresse}, function(results:any) {
+        map.setZoom(18);
+        map.setCenter(results[0].geometry.location);
+          
+      // Création du marqueur du lieu (épingle)
+          marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location,
+          draggable: true,
+          title: "precisez votre position"
+      });
+      // la chaine caractère que contient infoswindows
+      const contentString =
+      '<div id="content">' +
+      '<div id="siteNotice">' +
+      "</div>" +
+      '<h1 id="">'+adresse+'</h1>' +'</div>'+'</div>'
+      marker.bindTo('bounds',map);
+      marker.setVisible(true);
+      const infowindow = new google.maps.InfoWindow({
+        content: contentString,
+      });
+      marker.addListener('click', function() { //ajout du listener sur le marquer l'evenement attendu est un click
+        infowindow.open(map, marker);
+      })
+    });
   }
 }
